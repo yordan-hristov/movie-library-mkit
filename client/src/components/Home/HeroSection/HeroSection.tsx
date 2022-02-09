e7b3ba1fcaba23a1317ad0ee561f7d7cb5c5be23
Yordan Hristov <y.hristov11@gmail.com>
2022-02-09 18:18:35 +0200

    Add logout

--- client/src/components/Home/HeroSection/HeroSection.tsx
+++ client/src/components/Home/HeroSection/HeroSection.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { removeUser } from '../../../store/user/userSlice';
 
 import './HeroSection.scss';
 
 const HeroSection = () => {
+    const dispatch = useDispatch();
+
+    const handleClick = () => {
+        dispatch(removeUser());
+    }
+
     return <section className='hero-section'>
         <h1 className="hero-section-heading">My Movie Library</h1>
         <p className="hero-section-text">Manage and review your favorite movies</p>
         <Link to={'/search'}><button className="hero-section-button">Search</button></Link>
+        <div className="logout" onClick={handleClick}>Logout</div>
     </section>;
 };
 
