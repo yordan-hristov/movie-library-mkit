8661b2dee90f28ffa6eb8c3b9b58fb4a6e0cd66e
Yordan Hristov <y.hristov11@gmail.com>
2022-02-09 20:52:40 +0200

    Add LoadingSpinner to images

--- client/src/components/shared/MovieImageCard/MovieImageCard.tsx
+++ client/src/components/shared/MovieImageCard/MovieImageCard.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 
 import './MovieImageCard.scss';
 
@@ -8,9 +9,18 @@ type MovieImageCardProps = {
   movieId: string;
 }
 
-const MovieImageCard = ({imageUrl, movieId}:  MovieImageCardProps) => {
+const MovieImageCard = ({ imageUrl, movieId }: MovieImageCardProps) => {
+  const [loaded, setLoaded] = useState(false);
+
   return <div className='movie-image-card'>
-      <Link to={`/movies/${movieId}`}><img src={imageUrl}></img></Link>
+    {!loaded && <LoadingSpinner />}
+    <Link to={`/movies/${movieId}`}>
+      <img
+        src={imageUrl}
+        onLoad={() => setLoaded(true)}
+        style={loaded ? {} : {display: 'none'}}
+      ></img>
+    </Link>
   </div>;
 };
 
