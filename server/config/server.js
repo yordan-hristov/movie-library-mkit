f410b9d9ba42c7bb7f53d8f3d73a8a611ddb56e7
Yordan Hristov <y.hristov11@gmail.com>
2022-02-11 14:54:17 +0200

    Change db from local to mongo atlas

--- server/config/server.js
+++ server/config/server.js
@@ -1,6 +1,6 @@
 const serverConfig = {
     PORT: 5000,
-    DB_CONNECTION: 'mongodb://localhost:27017/movieLibrary',
+    DB_CONNECTION: 'mongodb+srv://yordan11:GRIfxj9zYl5M6IBn@bookandmoviemanager.kdqnj.mongodb.net/movieLibrary?retryWrites=true&w=majority',
 };
     
 
